213ceeaae89d92c1385f282d4a609d0593bbed71
Henrik <hg04@web.de>
2024-11-06 10:35:10 +0100

    create customer

--- server/core/customer.js
+++ server/core/customer.js
@@ -3,7 +3,7 @@ export function getAllCustomers(fastify) {
 
     try {
         const customers = statement.all();
-        return { customers };
+        return {customers};
     } catch (error) {
         fastify.log.error(error);
         return null;
@@ -15,6 +15,24 @@ export function getCustomer(fastify, id) {
 
     try {
         return statement.get(id);
+    } catch (error) {
+        fastify.log.error(error);
+        return null;
+    }
+}
+
+export function createCustomer(fastify, customer) {
+    const statement = fastify.db.prepare('INSERT INTO customers (name, email, phone, address, city, country, zip, company) VALUES (?, ?, ?, ?, ?, ?, ?, ?)');
+
+    try {
+        const result = statement.run(customer.name, customer.email, customer.phone, customer.address, customer.city, customer.country, customer.zip, customer.company);
+        const customerId = result.changes === 1 ? result.lastInsertRowid : null;
+        if (customerId) {
+            return getCustomer(fastify, customerId);
+        } else {
+            fastify.log.error('Could not create customer');
+            return null;
+        }
     } catch (error) {
         fastify.log.error(error);
         return null;
--- server/routes/customers.routes.js
+++ server/routes/customers.routes.js
@@ -1,6 +1,10 @@
 import { customerSchema } from "../schemas/customer.schema.js";
 
-import { getAllCustomers } from "../core/customer.js";
+import {
+    createCustomer,
+    getAllCustomers,
+    getCustomer
+} from "../core/customer.js";
 
 
 async function CustomersRoutes(fastify) {
@@ -11,6 +15,36 @@ async function CustomersRoutes(fastify) {
             return { error: "Internal Server Error" };
         }
         return customers;
+    });
+
+    fastify.get('/:id', customerSchema, async (request, reply) => {
+        const customer = getCustomer(fastify, request.params.id);
+        if (!customer) {
+            reply.code(404);
+            return { error: "Customer not found" };
+        }
+        return customer;
+    });
+
+    fastify.post('/', customerSchema, async (request, reply) => {
+        const customerProps = request.body;
+        const customer = {
+            name: customerProps.name,
+            email: customerProps.email,
+            phone: customerProps.phone,
+            address: customerProps.address,
+            city: customerProps.city,
+            country: customerProps.country,
+            zip: customerProps.zip,
+            company: customerProps.company
+        }
+        const newCustomer = createCustomer(fastify, customer);
+        if (!newCustomer) {
+            reply.code(500);
+            return { error: "Internal Server Error" };
+        }
+        reply.code(201);
+        return newCustomer;
     });
 }
 
