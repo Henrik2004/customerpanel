03ca1dbc07de466de3ed2b567e7f1a559b2ba19d
Henrik <hg04@web.de>
2024-11-28 15:08:25 +0100

    switched client to angular

--- client/src/app/settings/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function SettingsPage() {
-    return (
-        <div className="content-container">
-        <h1>Einstellungen</h1>
-        <p>Einstellungen-Seite Inhalt</p>
-        </div>
-    );
-}
