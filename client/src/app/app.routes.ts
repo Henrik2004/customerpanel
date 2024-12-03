2602c8b7add693f33e7509f40dc00c76b27d9401
Henrik <hg04@web.de>
2024-12-03 16:10:15 +0100

    frontend start

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import {OverviewComponent} from './overview/overview.component';
+import {OffersComponent} from './offers/offers.component';
+import {CustomersComponent} from './customers/customers.component';
+import {SettingsComponent} from './settings/settings.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'overview', component: OverviewComponent },
+  { path: 'offers', component: OffersComponent },
+  { path: 'customers', component: CustomersComponent },
+  { path: 'settings', component: SettingsComponent },
+  { path: '', redirectTo: '/overview', pathMatch: 'full' }
+];
