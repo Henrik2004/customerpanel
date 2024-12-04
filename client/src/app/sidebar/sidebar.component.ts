ed0bbd00789a34b6cfb047e6394cd7e45d945d7a
Henrik <hg04@web.de>
2024-12-04 12:40:45 +0100

    layout

--- client/src/app/sidebar/sidebar.component.ts
+++ client/src/app/sidebar/sidebar.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import {Router, RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [
+    RouterLink,
+    RouterLinkActive
+  ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent {
+  constructor(private router: Router) {}
 
+  isActive(url: string): boolean {
+    return this.router.url === url;
+  }
 }
