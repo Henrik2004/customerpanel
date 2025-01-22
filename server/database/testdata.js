76c2810a7f52b4432ef8be1fa1fa60618aa7709f
Henrik <hg04@web.de>
2025-01-22 20:04:51 +0100

    new test data

--- server/database/testdata.js
+++ server/database/testdata.js
@@ -1,23 +1,23 @@
 export const customers = [{
-    name: 'Baumer Baum 2',
-    email: 'test@test.de',
+    name: 'Mario Pfeiffer',
+    email: 'mario.pfeiffer@gmail.com',
     phone: '01234567',
-    address: 'Baumstraße 7',
-    city: 'Baumstadt',
-    country: 'Baumland',
+    address: 'Pfeifferstraße 7',
+    city: 'Pfeifferstadt',
+    country: 'Pfeifferland',
     zip: '2839',
-    company: 'Baumann',
+    company: 'Pfeiffer GmbH',
     createdBy: 1,
 }, {
-    name: 'Baumer Baum 3',
-    email: 'test3@test.de',
-    phone: '01234567',
-    address: 'Baumstraße 7',
-    city: 'Baumstadt',
-    country: 'Baumland',
+    name: 'Petra Meier',
+    email: 'petra.meier@web.de',
+    phone: '98765432',
+    address: 'Meierstraße 9',
+    city: 'Meierstadt',
+    country: 'Meierland',
     zip: '2839',
-    company: 'Baumann',
-    createdBy: 1
+    company: 'Meier GmbH',
+    createdBy: 1,
 }]
 
 export const roles = [{
@@ -38,19 +38,19 @@ export const roles = [{
 }]
 
 export const users = [{
-    name: 'Test Account Manager',
+    name: 'Accountmanager',
     password: 'test',
     role: 1,
     createdBy: 1,
     updatedBy: 1
 }, {
-    name: 'Test Developer',
+    name: 'Developer',
     password: 'test',
     role: 2,
     createdBy: 1,
     updatedBy: 1
 }, {
-    name: 'Test User',
+    name: 'User',
     password: 'test',
     role: 3,
     createdBy: 1,
@@ -59,16 +59,16 @@ export const users = [{
 
 export const offers = [{
     customerId: 1,
-    title: 'Test Offer',
-    description: 'Test Offer Description',
+    title: 'Pfeiffer Offer',
+    description: 'Pfeiffer Offer Description',
     price: 100,
     status: 'active',
     createdBy: 1,
     updatedBy: 1
 }, {
     customerId: 2,
-    title: 'Test Offer 2',
-    description: 'Test Offer Description 2',
+    title: 'Meier Offer',
+    description: 'Meier Offer Description',
     price: 200,
     status: 'draft',
     createdBy: 1,
